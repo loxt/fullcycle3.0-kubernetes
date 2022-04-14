1d408641213159a93bc019d91764505eb73460cf
loxt <emannuelloxt@hotmail.com>
2022-04-13 23:38:26 -0400

    feat: add secret environment variables

--- server.go
+++ server.go
@@ -9,25 +9,32 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-  name := os.Getenv("NAME")
-  age := os.Getenv("AGE")
-
-  fmt.Fprintf(w, "Hello %s! You are %s years old.", name, age)
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	
+	fmt.Fprintf(w, "Hello %s! You are %s years old.", name, age)
 }
 
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s!. Password: %s", user, password)
+	
+}
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
-  data, err := ioutil.ReadFile("/go/orders/orders.txt")
-
-  if err != nil {
-    log.Fatalf("Error reading file: ", err)
-  }
-
-  fmt.Fprintf(w, "My orders: %s!", string(data))
+	data, err := ioutil.ReadFile("/go/orders/orders.txt")
+	
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	
+	fmt.Fprintf(w, "My orders: %s!", string(data))
 }
