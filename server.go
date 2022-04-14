e08aecfbf2cf4890df8172ae07cfddcecacb1fb6
loxt <emannuelloxt@hotmail.com>
2022-04-13 22:56:43 -0400

    feat: add environment variables

--- server.go
+++ server.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+  "fmt"
+  "os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
@@ -8,5 +12,9 @@ func main() {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+  name := os.Getenv("NAME")
+  age := os.Getenv("AGE")
+
+  fmt.Fprintf(w, "Hello %s! You are %s years old.", name, age)
 	w.Write([]byte("<h1>Hello Full Cycle!</h1>"))
 }
